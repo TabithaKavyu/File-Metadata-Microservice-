f41bf07667e1aa271ae19fe06ea88ad6bf83665a
Glitch (stone-adorable-dreadnought) <none>
2020-04-11 22:52:48 +0000

    📰⛸ Updated with Glitch

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
+// require and use "multer"...
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.post('/api/analysefile', upload.single('img'), function(req, res){
+  console.log(req.file);
+
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
